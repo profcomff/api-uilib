3afcbbf1ecbff4557f2be5553c7e0f18c58d9e3c
default <dyakovri@profcomff.com>
2024-07-25 19:48:22 +0000

    Better package

---
+++ src/index.d.ts
@@ -0,0 +1,24 @@
+import Client from "openapi-fetch";
+import type { paths as achievementPaths } from "./openapi/achievement.ts";
+import type { paths as authPaths } from "./openapi/auth.ts";
+import type { paths as marketingPaths } from "./openapi/marketing.ts";
+import type { paths as pingerPaths } from "./openapi/pinger.ts";
+import type { paths as printPaths } from "./openapi/print.ts";
+import type { paths as servicesPaths } from "./openapi/services.ts";
+import type { paths as socialPaths } from "./openapi/social.ts";
+import type { paths as timetablePaths } from "./openapi/timetable.ts";
+import type { paths as userdataPaths } from "./openapi/userdata.ts";
+
+export type paths = (
+    achievementPaths &
+    authPaths &
+    marketingPaths &
+    pingerPaths &
+    printPaths &
+    servicesPaths &
+    socialPaths &
+    timetablePaths &
+    userdataPaths
+);
+export function createClient(baseUrl: string): ReturnType<typeof Client>;
+export function setupAuth(newToken: string | null): void;
