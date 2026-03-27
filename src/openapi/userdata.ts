3ce955ce207e99611d630ffb8d91eb700bc61139
Dmatrushka19 <102908513+Dmatrushka19@users.noreply.github.com>
2026-03-27 21:44:30 +0000

    API codegen update

--- src/openapi/userdata.ts
+++ src/openapi/userdata.ts
@@ -471,6 +471,10 @@ export interface components {
         };
         /** ValidationError */
         ValidationError: {
+            /** Context */
+            ctx?: Record<string, never>;
+            /** Input */
+            input?: unknown;
             /** Location */
             loc: (string | number)[];
             /** Message */
